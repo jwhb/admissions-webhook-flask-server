b19103c8968a80c59f809bbca97dc33389049f4f
jwiegratz <jasper.wiegratz@sva.de>
2022-12-16 15:12:44 +0100

    fix tail in boot.sh if log file is not present

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 gunicorn -D -b :5001 --access-logfile app.log --error-logfile app.log --certfile=${CERT:-/tls/cert} --keyfile=${KEY:-/tls/key} admissions-webhook:app
 gunicorn -D -b :5000 --access-logfile app.log --error-logfile app.log admissions-webhook:app
-tail -f app.log
+tail -F app.log
